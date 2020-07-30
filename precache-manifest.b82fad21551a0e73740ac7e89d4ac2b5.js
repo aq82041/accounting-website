6f68e83e0658c0caae7281b4fbecb3b97f739e75
anqi <18742024663@163.com>
2020-07-30 17:45:46 +0800

    deploy

---
+++ precache-manifest.b82fad21551a0e73740ac7e89d4ac2b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588157b965157cad8dbf",
+    "url": "/accounting-website/css/app.4993af7c.css"
+  },
+  {
+    "revision": "e369779bfcea95e3a56efb31ccb73eed",
+    "url": "/accounting-website/index.html"
+  },
+  {
+    "revision": "588157b965157cad8dbf",
+    "url": "/accounting-website/js/app.68b8372a.js"
+  },
+  {
+    "revision": "c926a1c4fe359326a533",
+    "url": "/accounting-website/js/chunk-vendors.a1c48f68.js"
+  },
+  {
+    "revision": "e09939e8bdb3bc07e427e0d8dbf41c91",
+    "url": "/accounting-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/accounting-website/robots.txt"
+  }
+]);
